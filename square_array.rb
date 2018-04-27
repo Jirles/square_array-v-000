cce472b4db938e6edc93822706b38c61a6c0b577
Emily Jirles <emily.jirles@gmail.com>
2018-04-27 17:39:10 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,15 @@
 def square_array(array)
   result = []
   array.each {|i| result << i**2}
-  puts result
+  result
 end
 
 def square_array_collect(array)
-  puts array.collect {|i| i**2}
+  array.collect {|i| i**2}
 end
 
+with_each = square_array([1, 2, 3, 4])
 puts "square_array output:"
-square_array([1, 2, 3, 4])
+with_each
 puts "using #collect:"
 square_array_collect([1,2,3,4])
